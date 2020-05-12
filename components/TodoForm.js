3f61c887dc480fbbada5324405e3a83a25b7cb57
isaac stott <isaac.cloyd@gmail.com>
2020-05-11 18:42:39 -0600

    moved files

---
+++ components/TodoForm.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const TodoForm = (props) => {
+
+  const handleChanges = e => {
+    // update state with each keystroke
+    props.setItem(
+      e.target.value
+    );
+    console.log(props.item)
+  };
+
+    return (
+      <form onSubmit={props.addTask}>
+        <input
+          type="text"
+          name="item"
+          value={props.item}
+          placeholder= " ...to-do"
+          onChange={handleChanges}
+        />
+        <button>Add to-do</button>
+      </form>
+    );
+}
+
+export default TodoForm;
